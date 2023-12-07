9be6072202175a767ff010e3624ecee798c1076e
abhay3366 <tabhaykant2@gmail.com>
2023-12-07 17:40:25 +0530

    add readme file

--- README.md
+++ README.md
@@ -15,12 +15,19 @@
 
 # for form where large number of fields than use "formik.org library"
 # features
-
     -Login and signup
       - Singn In /Signup PAGE
       - REDIRECT TO BROWSER PAGE
-    -MainMovies
-        -Trailers
-        -Title and Description
-        - Movies sugghestion
-        - Movies list
+
+    -Browse(after authentication)
+      -Headers
+      -MainMovies
+          -Trailers
+          -Title and Description
+          - Movies sugghestion
+            - MoviesList *N
+         
+
+    -NetflixGpt
+      -searchbar
+      -Movie suggestions
---
+++ cbse.php
@@ -0,0 +1,38 @@
+
+<?php require('header.php')?>
+<div class="outerdiv">
+        <div class="innerdiv">
+        	<div class="content_panel">
+                                         
+				<div class="post" id="post-467">
+        <div class="mas_heading"><h1>CBSE Mandatory Public Disclosure</h1></div>
+        <div class="mas_lineBox">
+        	<span class="yelloLine"></span>
+            <span class="skyLine"></span>
+        </div>
+        <!--<div class="line"><img src="/wp-content/themes/eisp/images/line_dark.png" /></div>-->
+		
+			<div class="entry">
+				<table border="1" class="table table-bordered table-responsive" width="100%">
+<tbody class="text-color-blue">
+<tr>
+<td align="center" height="40" valign="middle"><strong>SL NO.</strong></td>
+<td align="center" valign="middle"><strong>INFORMATION</strong></td>
+<td align="center" valign="middle"><strong>DETAILS</strong></td>
+</tr>
+<tr class="tr-bg-color">
+<td align="center" height="21" valign="middle">1.</td>
+<td align="left" valign="middle">Link to PDF</td>
+<td align="center" valign="middle"><a href="../Mandatory%20Disclosure%20Details%20_%20SARAS%204.0.pdf" target="_blank" rel="noopener">Link</a></td>
+</tr>
+</tbody>
+</table>
+							</div>
+		</div>
+		   
+   </div>
+    	</div>
+    </div>
+
+
+    <?php require('footer.php')?>
